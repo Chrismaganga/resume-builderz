858e007ac9cf8e0328af7d9d665129de95c27f8d
Chrismaganga <chrismaganga3@gmail.com>
2025-09-24 19:37:54 +0200

    updated speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const rootElement = document.getElementById('app')!
 const root = createRoot(rootElement)
@@ -9,5 +10,6 @@ const root = createRoot(rootElement)
 root.render(
   <StrictMode>
     <App />
+    <SpeedInsights/>
   </StrictMode>,
 )
